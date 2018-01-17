2bf5b681c9ef414675c1f4af2b6fe1a3b2ed9753
ashleycano <acano6835@gmail.com>
2018-01-17 11:52:25 -0600

    division portion of exercise

--- src/main/java/com/codeup/springbootblog/MathController.java
+++ src/main/java/com/codeup/springbootblog/MathController.java
@@ -10,22 +10,28 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MathController {
 
-//    @RequestMapping (path = "/add/{number}/and/{num}", method = RequestMethod.GET)
-//    @ResponseBody
-//    public String add(@PathVariable int number, @PathVariable int num) {
-//        return number + " plus " + num + " is " + (number + num) + "!";
-//    }
+    @RequestMapping (path = "/add/{number}/and/{num}", method = RequestMethod.GET)
+    @ResponseBody
+    public String add(@PathVariable int number, @PathVariable int num) {
+        return number + " plus " + num + " is " + (number + num) + "!";
+    }
 
     @RequestMapping (path = "/subtract/{number}/from/{num}", method = RequestMethod.GET)
     @ResponseBody
     public String subtract(@PathVariable int number, @PathVariable int num) {
-        return number+ " minus " + num + " is " + (num - number) + "!";
+        return number + " minus " + num + " is " + (num - number) + "!";
     }
 
     @RequestMapping (path = "/multiply/{number}/and/{num}", method = RequestMethod.GET)
     @ResponseBody
     public String multiply(@PathVariable int number, @PathVariable int num) {
         return number + " times " + num + " is " + (number * num) + "!";
+    }
+
+    @RequestMapping (path = "/divide/{number}/by/{num}", method = RequestMethod.GET)
+    @ResponseBody
+    public String divide(@PathVariable int number, @PathVariable int num) {
+        return number + " divided by " + num + " is " + (number / num) + "!";
     }
 
 }
