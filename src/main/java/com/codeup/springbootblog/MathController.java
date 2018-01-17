21d35a623a4080b4904fa356df5210ea9c11d384
ashleycano <acano6835@gmail.com>
2018-01-17 11:44:28 -0600

    subtraction portion of exercise

--- src/main/java/com/codeup/springbootblog/MathController.java
+++ src/main/java/com/codeup/springbootblog/MathController.java
@@ -10,10 +10,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MathController {
 
-    @RequestMapping (path = "/add/{number}/and/{num}", method = RequestMethod.GET)
+//    @RequestMapping (path = "/add/{number}/and/{num}", method = RequestMethod.GET)
+//    @ResponseBody
+//    public String add(@PathVariable int number, @PathVariable int num) {
+//        return number + " plus " + num + " is " + (number + num) + "!";
+//    }
+
+    @RequestMapping (path = "/subtract/{number}/from/{num}", method = RequestMethod.GET)
     @ResponseBody
-    public String addOne(@PathVariable int number, @PathVariable int num) {
-        return number + " plus " + num + " is " + (number + num) + "!";
+    public String subtract(@PathVariable int number, @PathVariable int num) {
+        return number+ " minus " + num + " is " + (num - number) + "!";
     }
 
 }
