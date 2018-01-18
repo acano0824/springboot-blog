4fdc7a19270990554668e817cdb92f45e9350f79
ashleycano <acano6835@gmail.com>
2018-01-18 13:49:14 -0600

    roll dice exercise

--- src/main/java/com/codeup/springbootblog/HomeController.java
+++ src/main/java/com/codeup/springbootblog/HomeController.java
@@ -1,18 +1,24 @@
 package com.codeup.springbootblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/landing")
-    @ResponseBody
-    public String hello() {
-        return "This is the landing page!";
+//    @GetMapping("/home")
+//    public String welcome() {
+//        return "home";
+//    }
+
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable String name, Model viewModel) {
+        viewModel.addAttribute("name", name);
+        return "hello";
     }
 
 }
---
+++ src/main/java/com/codeup/springbootblog/RollDiceController.java
@@ -0,0 +1,48 @@
+package com.codeup.springbootblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class RollDiceController {
+
+    private int counter = 0;
+
+    @GetMapping("/roll-dice")
+    public String showRollDice() {
+        return "roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{guess}")
+    public String play(@PathVariable int guess, Model viewModel) {
+
+
+        int random = (int)(Math.random() * 6 + 1);
+        counter++;
+
+
+        viewModel.addAttribute("guess", guess);
+        viewModel.addAttribute("random", random);
+        viewModel.addAttribute("counter", counter);
+
+        if(guess == random) {
+
+            counter = 0;
+
+        }
+        return "roll-dice";
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
